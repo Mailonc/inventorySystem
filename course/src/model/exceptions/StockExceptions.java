576c7c957d9558099d17ead2c7d9170075536521
Mailon Cesar <cmailon0410@gmail.com>
2026-05-16 11:52:50 -0300

    inventory system

---
+++ course/src/model/exceptions/StockExceptions.java
@@ -0,0 +1,12 @@
+package model.exceptions;
+
+public class StockExceptions extends Exception {
+
+	private static final long serialVersionUID = 1L; 
+	
+	public StockExceptions(String menssage) {
+		super(menssage);
+		
+	}
+
+}
